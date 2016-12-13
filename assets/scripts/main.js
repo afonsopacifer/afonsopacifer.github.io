1500cd729062e975ddbc912f9766f716d40308bd
afonsopacifer <afonsopolis@gmail.com>
2016-12-13 21:52:01 -0200

    build process added

---
+++ assets/scripts/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+(function () {
+  return console.log('ok');
+});
---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+// Modules :)
+// ===========================================
+const gulp   = require('gulp');
+const data   = require('gulp-data');
+const pug    = require('gulp-pug');
+const banana = require('gulp-banana');
+const babel  = require('gulp-babel');
+const connect = require('gulp-connect');
+
+// Compile Jade
+// ===========================================
+gulp.task('pug', () => {
+	gulp.src(['src/**.pug'])
+		  .pipe(data(file => require('./data-base/talks.json')))
+		  .pipe(data(file => require('./data-base/projects.json')))
+		  .pipe(data(file => require('./data-base/articles.json')))
+    	.pipe(pug())
+    	.pipe(gulp.dest('./'))
+    	.pipe(connect.reload());
+});
+
+// banana
+// ===========================================
+gulp.task('banana', () => {
+  gulp.src('src/styles/main.bnn')
+    .pipe(banana())
+    .pipe(gulp.dest('assets/styles/'))
+    .pipe(connect.reload());
+});
+
+// Babel
+// ===========================================
+gulp.task('babel', () => {
+  gulp.src('src/scripts/main.js')
+    .pipe(babel({
+      presets: ['es2015']
+     }))
+    .pipe(gulp.dest('assets/scripts/'))
+    .pipe(connect.reload());
+});
+
+// Watch
+// ===========================================
+gulp.task('watch', () => {
+	gulp.watch(['src/**/**.pug'], ['pug']);
+	gulp.watch(['src/styles/**/**.bnn'], ['banana']);
+  gulp.watch(['src/scripts/**.js'], ['babel']);
+});
+
+// Static server
+// ===========================================
+gulp.task('connect', () => {
+	connect.server({
+		root: './',
+		livereload: true
+	});
+});
+
+// More Tasks
+// ===========================================
+gulp.task('serve', ['connect', 'watch']);
+gulp.task('build', ['pug', 'banana', 'babel']);
