f21c7275377e09135e931e6509ea974fa1fb860e
afonsopacifer <afonsopolis@gmail.com>
2016-12-15 12:45:21 -0200

    :heartpulse: template structure created :heartpulse:

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,5 +1,8 @@
-'use strict';
-
-(function () {
-  return console.log('ok');
-});
+// var app = new senna.App();
+//     app.setBasePath('/');
+//     app.addSurfaces('contents');
+//     app.addRoutes([
+//   new senna.Route('index.html', senna.HtmlScreen),
+//   new senna.Route('talks.html', senna.HtmlScreen)
+//     ]);
+"use strict";
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
-() => console.log('ok');
+// var app = new senna.App();
+//     app.setBasePath('/');
+//     app.addSurfaces('contents');
+//     app.addRoutes([
+//   new senna.Route('index.html', senna.HtmlScreen),
+//   new senna.Route('talks.html', senna.HtmlScreen)
+//     ]);
