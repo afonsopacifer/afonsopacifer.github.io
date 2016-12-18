d1483a0c4ba9ada8c327ac9825526281264494f2
afonsopacifer <afonsopolis@gmail.com>
2016-12-18 01:52:59 -0200

    Iniciando Post sobre restrospecitva de 2016

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,12 +5,12 @@ document.body.addEventListener("mousemove", function (evt) {
   document.body.style.setProperty('--highlight-G', evt.clientY);
 });
 
-window.addEventListener('devicelight', function (event) {
-  if (event.value < 20) {
-    document.body.style.setProperty('--background-default', '#161616');
-    document.body.style.setProperty('--text-deafult', '#ffffff');
-  } else {
-    document.body.style.setProperty('--background-default', '#ffffff');
-    document.body.style.setProperty('--text-deafult', '#161616');
-  }
-});
+// window.addEventListener('devicelight', (event) => {
+//   if (event.value < 20) {
+//     document.body.style.setProperty('--background-default', '#161616');
+//     document.body.style.setProperty('--text-deafult', '#ffffff');
+//   } else {
+//     document.body.style.setProperty('--background-default', '#ffffff');
+//     document.body.style.setProperty('--text-deafult', '#161616');
+//   }
+// });
