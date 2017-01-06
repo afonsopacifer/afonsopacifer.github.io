6cd68f7d78310909fe36a7a264439903c1c7401d
afonsopacifer <afonsopolis@gmail.com>
2017-01-06 14:06:54 -0200

    adjustments for 2017 content

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,12 +3,12 @@ document.body.addEventListener("mousemove", (evt) => {
   document.body.style.setProperty('--highlight-G', evt.clientY);
 });
 
-// window.addEventListener('devicelight', (event) => {
-//   if (event.value < 20) {
-//     document.body.style.setProperty('--background-default', '#161616');
-//     document.body.style.setProperty('--text-deafult', '#ffffff');
-//   } else {
-//     document.body.style.setProperty('--background-default', '#ffffff');
-//     document.body.style.setProperty('--text-deafult', '#161616');
-//   }
-// });
+window.addEventListener('devicelight', (event) => {
+  if (event.value < 20) {
+    document.body.style.setProperty('--background-default', '#161616');
+    document.body.style.setProperty('--text-deafult', '#ffffff');
+  } else {
+    document.body.style.setProperty('--background-default', '#ffffff');
+    document.body.style.setProperty('--text-deafult', '#161616');
+  }
+});
