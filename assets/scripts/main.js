b400eb152719f85466872a6243c4e150fcc3e0db
afonsopacifer <afonsopolis@gmail.com>
2017-01-17 16:24:23 -0200

    updates for first MVP

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,16 +1,15 @@
-'use strict';
+// document.body.addEventListener("mousemove", (evt) => {
+//   document.body.style.setProperty('--highlight-R', evt.clientX);
+//   document.body.style.setProperty('--highlight-G', evt.clientY);
+// });
 
-document.body.addEventListener("mousemove", function (evt) {
-  document.body.style.setProperty('--highlight-R', evt.clientX);
-  document.body.style.setProperty('--highlight-G', evt.clientY);
-});
-
-window.addEventListener('devicelight', function (event) {
-  if (event.value < 20) {
-    document.body.style.setProperty('--background-default', '#161616');
-    document.body.style.setProperty('--text-deafult', '#ffffff');
-  } else {
-    document.body.style.setProperty('--background-default', '#ffffff');
-    document.body.style.setProperty('--text-deafult', '#161616');
-  }
-});
+// window.addEventListener('devicelight', (event) => {
+//   if (event.value < 20) {
+//     document.body.style.setProperty('--background-default', '#161616');
+//     document.body.style.setProperty('--text-deafult', '#ffffff');
+//   } else {
+//     document.body.style.setProperty('--background-default', '#ffffff');
+//     document.body.style.setProperty('--text-deafult', '#161616');
+//   }
+// });
+"use strict";
