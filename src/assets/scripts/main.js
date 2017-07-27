c3cd540bedfef178ab782ba1fb1d501981bd697e
afonsopacifer <afonsopolis@gmail.com>
2017-07-27 16:58:06 -0300

    Ambientlight on

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,14 +1,14 @@
-// window.addEventListener('devicelight', (e) => {
-//   if (e.value < 20) {
-//     document.body.style.setProperty('--background-default', '#282828');
-//     document.body.style.setProperty('--text-deafult', '#ffffff');
-//     document.body.style.setProperty('--complementary', '#c9c9c9');
-//   } else {
-//     document.body.style.setProperty('--background-default', '#ffffff');
-//     document.body.style.setProperty('--text-deafult', '#282828');
-//     document.body.style.setProperty('--complementary', '#949494');
-//   }
-// });
+window.addEventListener('devicelight', (e) => {
+  if (e.value < 20) {
+    document.body.style.setProperty('--background-default', '#282828');
+    document.body.style.setProperty('--text-deafult', '#ffffff');
+    document.body.style.setProperty('--complementary', '#c9c9c9');
+  } else {
+    document.body.style.setProperty('--background-default', '#ffffff');
+    document.body.style.setProperty('--text-deafult', '#282828');
+    document.body.style.setProperty('--complementary', '#949494');
+  }
+});
 
 Mousetrap.bind('up up down down left right left right b a',() => {
     document.body.addEventListener("mousemove", (e) => {
