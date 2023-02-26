e31247da6f6c2d94cfec0660ada5d6bc76474ab0
afonsopacifer <afonsopolis@gmail.com>
2023-02-26 17:43:16 -0300

    feat(views): pages/articles allow filter by lang

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DefaultLayout from '../templates/default-layout';
 import '../atoms/headings/headings.css';
 import '../atoms/text/text.css';
@@ -11,44 +11,64 @@ import Link from '../atoms/link/';
 import PageHeader from '../organisms/page-header/';
 import Dev from '../assets/icons/dev.svg';
 import Medium from '../assets/icons/medium.svg';
+import Filter from '../molecules/filter/';
+import filterRules from '../bosons/helpers/filterRules/filterRules';
 
-const Articles = () => (
-  <DefaultLayout>
+const Articles = () => {
+  
+  const [filters, setFilters] = useState([]);
 
-    <SEO title = 'Articles' />
+  return (
+    <DefaultLayout>
 
-    <PageHeader>
+      <SEO title = 'Articles' />
 
-      <h1 className = 'heading heading--lv1'>
-        <strong className = 'heading--underline'> Blog posts </strong> about the tech world.
-      </h1>
+      <PageHeader>
 
-      <p className = 'text'>
-        I started writing to record my journey through the <a href='https://johnresig.com/blog/write-code-every-day/' className = 'link'>write code every day</a> challenge in 2015
-        <em className = 'text--hand-drawing'>
-          &nbsp;(inpired by <a href='https://raphamorim.io/about/' target = '_blank' className = 'link'>Raphael Amorim</a> and <a href='https://johnresig.com/about/' target = '_blank' className = 'link'>John Resig</a>) 
-        </em>.
-      </p>
+        <h1 className = 'heading heading--lv1'>
+          <strong className = 'heading--underline'> Blog posts </strong> about the tech world.
+        </h1>
 
-      <p className = 'text'>
-        Many years later, here are my beloved articles about engineering, design and career.
-      </p>
+        <p className = 'text'>
+          I started writing to record my journey through the <a href='https://johnresig.com/blog/write-code-every-day/' className = 'link'>write code every day</a> challenge in 2015
+          <em className = 'text--hand-drawing'>
+            &nbsp;(inpired by <a href='https://raphamorim.io/about/' target = '_blank' className = 'link'>Raphael Amorim</a> and <a href='https://johnresig.com/about/' target = '_blank' className = 'link'>John Resig</a>) 
+          </em>.
+        </p>
 
-      <Link href = 'https://dev.to/afonsopacifer/'> 
-          <Dev />
-          Follow me on DEV / <b className = 'text--highlight'>+39k post views</b>
-      </Link>
+        <p className = 'text'>
+          Many years later, here are my beloved articles about engineering, design and career.
+        </p>
 
-      <Link href = 'https://medium.com/@afonsopacifer'> 
-          <Medium />
-          Follow me on Medium / <b className = 'text--highlight'>+500 Followers</b>
-      </Link>
+        <Link href = 'https://dev.to/afonsopacifer/'> 
+            <Dev />
+            Follow me on DEV / <b className = 'text--highlight'>+39k post views</b>
+        </Link>
 
-    </PageHeader>
+        <Link href = 'https://medium.com/@afonsopacifer'> 
+            <Medium />
+            Follow me on Medium / <b className = 'text--highlight'>+500 Followers</b>
+        </Link>
 
-    { renderSections(articles) }
+      </PageHeader>
 
-  </DefaultLayout>
-);
+      <Filter
+        label = 'Filter by lang'
+        id = 'lang'
+        setFilters = { setFilters }
+        filters = { filters }
+        options = {[
+          { label: 'Portuguese', id: 'pt-br' },
+          { label: 'English', id: 'en' },
+          { label: 'Chinese', id: 'zh' },
+        ]}
+      />
+
+      { renderSections(articles, filterRules(filters, 'lang')) }
+
+    </DefaultLayout>
+  );
+
+}
 
 export default Articles;
